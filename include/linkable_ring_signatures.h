83b7dc254d082c5804e7f820a906007a67875d26
apuljain <18apul@gmail.com>
2012-06-15 01:18:51 -0400

    source files added

---
+++ include/linkable_ring_signatures.h
@@ -0,0 +1,37 @@
+#include <cryptopp/hex.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/base64.h>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <set>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <gmp.h>
+#include <cryptopp/integer.h>
+#include <cryptopp/modarith.h>
+#include <iomanip>
+#include <cryptopp/randpool.h>
+#include <cryptopp/filters.h>
+#include <vector>
+#include <cassert>
+
+#include <stdexcept>
+using std::runtime_error;
+
+#include <cryptopp/osrng.h>
+using CryptoPP::AutoSeededRandomPool;
+
+#include <cryptopp/nbtheory.h>
+using CryptoPP::ModularExponentiation;
+
+#include <cryptopp/dh.h>
+using CryptoPP::DH;
+
+#include <cryptopp/secblock.h>
+using CryptoPP::SecByteBlock;
+
+using namespace std;
+using namespace CryptoPP;
+
---
+++ makefile
@@ -0,0 +1,17 @@
+CC = g++
+CFLAGS = -c
+SRC = ./src
+BIN = ./bin
+OBJ = ./obj
+
+all: ringsign
+	 @echo "'ringsign' executable created in bin directory.."
+
+ringsign : linkable_ring_signatures.o
+		   $(CC) $(OBJ)/linkable_ring_signatures.o -lcryptopp -o $(BIN)/ringsign
+
+linkable_ring_signatures.o :
+		   	$(CC) $(CFLAGS) $(SRC)/linkable_ring_signatures.cpp -o $(OBJ)/linkable_ring_signatures.o
+
+clean :	
+		rm -rf $(OBJ)/*o $(BIN)/ringsign
